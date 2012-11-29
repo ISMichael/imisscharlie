fb993c4ea76f05b40196a569b947acb31c75b7ac
Michael <kuznetsov@gmail.com>
2012-11-28 22:04:21 -0500

    added lots of logic. lets see what breaks

--- src/i_miss_charlie/counter/views.py
+++ src/i_miss_charlie/counter/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from counter.models import Visit
+from django.shortcuts import render_to_response
+
+def index(request):
+	v = Visit.objects.get(id=1).visit_date
+	return render_to_response('counter/index.html',{'days':v})
+
+
