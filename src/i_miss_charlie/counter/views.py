22c962d52eaf6d7590da2f993ac93d23094489d4
Michael <kuznetsov@gmail.com>
2012-11-28 22:38:22 -0500

    adjusting date logic

--- src/i_miss_charlie/counter/views.py
+++ src/i_miss_charlie/counter/views.py
@@ -1,8 +1,12 @@
 from counter.models import Visit
 from django.shortcuts import render_to_response
+from datetime import datetime
 
 def index(request):
+	today = datetime.date.today()
 	v = Visit.objects.get(id=1).visit_date
-	return render_to_response('counter/index.html',{'days':v})
+	delta = today-v
+	days = delta.days
+	return render_to_response('counter/index.html',{'days':days})
 
 
